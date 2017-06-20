5ad2fa55dc93f1c89e69bc08fa998af47e53aa12
Chris Brooks <chris@chrisbrooks.org>
2017-06-20 14:18:32 -0400

    moved to postgres

--- keuka/settings.py
+++ keuka/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'keuka.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'keuka_dev',
+        'USER': 'keuka',
+        'PASSWORD': 'GxVsU23pjATf(e.Gv3nJJnRn&QiVAB',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
