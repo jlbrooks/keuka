3922518c1d8d58221559e134b6645a62d1aadb4c
Chris Brooks <chris@chrisbrooks.org>
2017-06-21 14:58:49 -0400

    trying to fix static file issue...

--- keuka/settings.py
+++ keuka/settings.py
@@ -140,9 +140,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'keuka', 'static'),
-)
 
 # Uploaded media (like badge images)
 
