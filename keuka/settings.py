6365da376c27607ed4bb495e3b34287281896c69
Chris Brooks <chris@chrisbrooks.org>
2017-06-21 15:07:46 -0400

    still static issues

--- keuka/settings.py
+++ keuka/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "keuka/static")
 
 # Uploaded media (like badge images)
 
