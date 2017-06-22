0d024612adc60a53e639ff900d3bbaf9fd15da88
Chris Brooks <chris@chrisbrooks.org>
2017-06-22 11:16:26 -0400

    fix https, plus hook in file storage

--- keuka/settings.py
+++ keuka/settings.py
@@ -19,6 +19,7 @@ AWS_STORAGE_BUCKET_NAME = 'keuka-assets'
 AWS_ACCESS_KEY_ID = 'AKIAIKYEYEMWZAE2DE7Q'
 AWS_SECRET_ACCESS_KEY = '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'
 AWS_S3_SECURE_URLS = False
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 # Tell django-storages that when coming up with the URL for an item in S3 storage, keep
 # it simple - just use this domain plus the path. (If this isn't set, things get complicated).
@@ -102,7 +103,7 @@ if 'RDS_DB_NAME' in os.environ:
     }
     # This is used by the `static` template tag from `static`, if you're using that. Or if anything else
     # refers directly to STATIC_URL. So it's safest to always set it.
-    STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+    STATIC_URL = "http://%s/" % AWS_S3_CUSTOM_DOMAIN
 
     # Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
     # you run `collectstatic`).
