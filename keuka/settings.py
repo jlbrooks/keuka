9436f5bff35c8af2423eab5b0b1a7be7716e5da6
Chris Brooks <chris@chrisbrooks.org>
2017-06-22 10:56:59 -0400

    oops, left code for dev in config

--- keuka/settings.py
+++ keuka/settings.py
@@ -166,14 +166,3 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-
-# Uploaded media (like badge images)
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'keuka/media')
